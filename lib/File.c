bf4bc4641843e098f39ac98d32c46b82e0f66e3d
jRimbault <jacques.rimbault@gmail.com>
2018-11-15 00:47:44 +0100

    write to file

--- lib/File.c
+++ lib/File.c
@@ -5,37 +5,48 @@
 #include "File.h"
 
 
-bool file_write_to(char* file, char* buffer)
+bool file_write_to(char* file, char* buffer, size_t size)
 {
-    return false;
+    FILE* fd = fopen(file, "wb");
+    if (fd == NULL) {
+        return false;
+    }
+    size_t written = fwrite(buffer, sizeof(char), size, fd);
+    if (!written) {
+        return false;
+    }
+    if (fclose(fd) != EOF) {
+        return false;
+    }
+    return true;
 }
 
 
 size_t file_read_to_buffer(char* file, char** buffer)
 {
     FILE* fd = fopen(file, "rb");
-    if (NULL == fd) {
+    if (fd == NULL) {
         return 0;
     }
     int rc = fseek(fd, 0L, SEEK_END);
-    if (0 != rc) {
+    if (rc != 0) {
         return 0;
     }
     long offEnd;
-    if (0 > (offEnd = ftell(fd))) {
+    if ((offEnd = ftell(fd)) < 0) {
         return 0;
     }
     size_t size = (size_t) offEnd;
     *buffer = calloc(size + 1, sizeof(char));
-    if (NULL == *buffer) {
+    if (*buffer == NULL) {
         return 0;
     }
     rewind(fd);
-    if (size != fread(*buffer, 1, size, fd)) {
+    if (fread(*buffer, 1, size, fd) != size) {
         free(*buffer);
         return 0;
     }
-    if (EOF == fclose(fd)) {
+    if (fclose(fd) == EOF) {
         free(*buffer);
         return 0;
     }
